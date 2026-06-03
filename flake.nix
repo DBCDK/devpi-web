70ea38f639a8f4e3f44555746044c4f14c2a7308
Jonathan Zielinski <jozi@dbc.dk>
2026-06-03 10:19:35 +0200

    flake: bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "devpi-server with the devpi-web plugin.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
